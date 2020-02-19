45ba2c8b4530fb0ccc63dd51d8d038db85e11345
Yoann Kergall <yoann.kergall@gmail.com>
2020-02-19 17:14:05 +0100

    ajout scripts bdd

---
+++ App_Data/data.sql
@@ -0,0 +1,7 @@
+--INSERT INTO TypeCuisine(Nom) VALUES('Indien'),('Asiatique'),('Pizza'),('Libanais'),('Boulangerie'),('Brasserie'),('Fast Food'),('Traditionnel FR')
+
+--INSERT INTO EtatCommande(Nom) VALUES('En Attente'),('En cours de préparation'),('En cours de livraison'),('Livré')
+
+--INSERT INTO TypeVersement(Nom, Statut) VALUES('Espèces', 1),('Chèque restaurant', 1)
+
+--INSERT INTO Categorie(Nom, Statut) VALUES('Entrée', 1),('Plat', 1),('Dessert', 1),('Salade', 1),('Boisson', 1),('Pâtes', 1),('Panini', 1)
